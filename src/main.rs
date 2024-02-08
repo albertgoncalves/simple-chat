5a8fedfd491bd4c2bcf759fb5537726bc4063c4e
albertgoncalves <goncalves.albert@gmail.com>
2024-02-07 21:56:50 -0500

    rename enum

--- src/main.rs
+++ src/main.rs
@@ -7,43 +7,43 @@ use std::thread;
 
 #[derive(Debug)]
 enum Comm {
-    ClientAdd(Arc<TcpStream>),
-    ClientRemove(Arc<TcpStream>),
-    Message(SocketAddr, String),
+    Connect(Arc<TcpStream>),
+    Close(Arc<TcpStream>),
+    Broadcast(SocketAddr, String),
 }
 
-fn client(stream: Arc<TcpStream>, sender: Sender<Comm>) {
+fn client(stream: &Arc<TcpStream>, sender: &Sender<Comm>) {
     println!("{stream:?}");
     let peer = stream.peer_addr().unwrap();
-    sender.send(Comm::ClientAdd(stream.clone())).unwrap();
+    sender.send(Comm::Connect(stream.clone())).unwrap();
     loop {
         let mut reader = BufReader::new(stream.as_ref());
         let mut string = String::new();
         match reader.read_line(&mut string) {
             Ok(0) | Err(_) => {
-                sender.send(Comm::ClientRemove(stream.clone())).unwrap();
+                sender.send(Comm::Close(stream.clone())).unwrap();
                 return;
             }
-            Ok(_) => sender.send(Comm::Message(peer, string)).unwrap(),
+            Ok(_) => sender.send(Comm::Broadcast(peer, string)).unwrap(),
         }
     }
 }
 
-fn server(receiver: Receiver<Comm>) {
+fn server(receiver: &Receiver<Comm>) {
     let mut clients = HashMap::new();
     loop {
         let comm = receiver.recv().unwrap();
         println!("{comm:?}");
         match comm {
-            Comm::ClientAdd(stream) => {
+            Comm::Connect(stream) => {
                 let peer = stream.peer_addr().unwrap();
                 let _ = clients.insert(peer, stream);
             }
-            Comm::ClientRemove(stream) => {
+            Comm::Close(stream) => {
                 let peer = stream.peer_addr().unwrap();
                 clients.remove(&peer).unwrap();
             }
-            Comm::Message(from, string) => {
+            Comm::Broadcast(from, string) => {
                 let message = format!("{from}: {string}");
                 for (to, stream) in &clients {
                     if from == *to {
@@ -61,13 +61,12 @@ fn main() {
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
     let (sender, receiver) = channel();
     thread::spawn(move || {
-        server(receiver);
+        server(&receiver);
     });
     for stream in listener.incoming() {
-        let stream = Arc::new(stream.unwrap());
         let sender = sender.clone();
         thread::spawn(move || {
-            client(stream, sender);
+            client(&Arc::new(stream.unwrap()), &sender);
         });
     }
 }
